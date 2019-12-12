f83ee263e20f484f73c8e823085fd658cb61edf2
mattShaverWebCreator <matt@boostthegram.com>
2019-12-12 08:26:32 -0500

    App Update: Adding Collection Preview Component, Shop Page, and relevant scss styles.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 
 function App() {
   return (
-    <HomePage />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
+    </Switch>
   );
 }
 
