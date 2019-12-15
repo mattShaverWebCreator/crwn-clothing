b70348e387cb64c0732576648fe50d45e4128ea7
mattShaverWebCreator <matt@boostthegram.com>
2019-12-14 20:32:38 -0500

    Added Header Component and Links for Shop, Contact, Home.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component.jsx';
+
 
 const HatsPage = () => (
   <div>
@@ -15,10 +17,13 @@ const HatsPage = () => (
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
