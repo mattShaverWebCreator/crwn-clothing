9508c6bdefe4a3124ea7c005f8c2cf929d845429
mattShaverWebCreator <matt@boostthegram.com>
2019-12-22 18:42:18 -0500

    Added Checkout Item Component, Styles. Updated Checkout component to pull in Checkout Item Component.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors.js';
 
 import "./checkout.styles.scss";
@@ -28,7 +30,7 @@ const CheckoutPage = ({cartItems, total}) => (
     </div>
     {
         cartItems.map(cartItem => 
-            cartItem.name
+           <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         )
     }
 
