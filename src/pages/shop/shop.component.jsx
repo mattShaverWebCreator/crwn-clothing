75d6ec8e6ed50b36e36ecd3d4f1be9386c0a77d6
mattShaverWebCreator <matt@boostthegram.com>
2020-01-05 09:47:51 -0500

    Completed adding Spinner to App, in order to show loading state for Collections, Collection Items when loading the Collections, Collection Items from Firestore.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -13,8 +13,19 @@ import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/fireb
 
 import { updateCollections } from '../../redux/shop/shop.actions';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 
 class ShopPage extends React.Component {
+
+  state = {
+    loading: true
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -23,17 +34,19 @@ class ShopPage extends React.Component {
 
     this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      console.log(collectionsMap);
+      updateCollections(collectionsMap);
+      this.setState({loading: false});
     });
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
 
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+        <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props}/>} />
+        <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props}/>} />
       </div>
     )
   }
@@ -41,8 +54,7 @@ class ShopPage extends React.Component {
 };
 
 const mapDispatchToProps = dispatch => ({
-  updateCollections: collectionsMap => 
-  dispatch(updateCollections(collectionsMap))
+  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
