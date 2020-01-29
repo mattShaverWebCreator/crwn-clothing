5340d3b477b0d31141737724697e2647c7912942
mattShaverWebCreator <razzabeanz@outlook.com>
2020-01-28 20:56:09 -0500

    Created SignUp Saga and completed adding Sagas to app.

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -11,7 +11,9 @@ import {
     signInSuccess, 
     signInFailure, 
     signOutSuccess, 
-    signOutFailure 
+    signOutFailure ,
+    signUpSuccess,
+    signUpFailure
 } from './user.actions';
 
 import {
@@ -73,6 +75,22 @@ export function* signOut() {
     }
 }
 
+export function* signUp({payload: { email, password, displayName }})  {
+    try {
+        const { user } = yield auth.createUserWithEmailAndPassword(
+            email,
+            password
+        );
+        yield put(signUpSuccess({ user, additionalData: { displayName } }))
+    } catch (error) { 
+        yield put(signUpFailure(error))
+    }
+}
+
+export function* signInAfterSignUp({payload: { user, additionalData }}) {
+    yield getSnapshotFromUserAuth(user, additionalData);
+}
+
 export function* onGoogleSignInStart() {
     yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -89,11 +107,21 @@ export function* onSignOutStart() {
     yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
 }
 
+export function* onSignUpStart() {
+    yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
+}
+
+export function* onSignUpSuccess() {
+    yield takeLatest(UserActionTypes.SIGN_UP_SUCCESS, signInAfterSignUp)
+}
+
 export function* userSagas() {
     yield all([
         call(onGoogleSignInStart), 
         call(onEmailSignInStart), 
         call(isUserAuthenticated),
-        call(onSignOutStart)
+        call(onSignOutStart),
+        call(onSignUpStart),
+        call(onSignUpSuccess)
     ]);
 }
