77b7a49fed6f8a5fb75846e5461088644d5fb11b
mattShaverWebCreator <matt@boostthegram.com>
2020-02-14 18:41:33 -0500

    Updated firebase configuration. Added styled components to collections overview, collections components. Added Container Components to Shop Component routes.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
 const ShopPage = ({ fetchCollectionsStart, match }) =>  {
@@ -15,7 +15,7 @@ const ShopPage = ({ fetchCollectionsStart, match }) =>  {
     return (
       <div className="shop-page">
         <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} /> 
+        <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} /> 
       </div>
     );
 };
